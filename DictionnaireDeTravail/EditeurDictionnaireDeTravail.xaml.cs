7e969f2b5675f4a582067bd6f4ebaaad640e71f9
Nicolas Pavie <pavie.nicolas@gmail.com>
2025-07-01 10:50:12 +0200

    chore: deplacement dans fichier séparé de la classe d'affichage de mot pour les datagris

--- DictionnaireDeTravail/EditeurDictionnaireDeTravail.xaml.cs
+++ DictionnaireDeTravail/EditeurDictionnaireDeTravail.xaml.cs
@@ -9,34 +9,6 @@ using System.Windows.Controls;
 
 namespace fr.avh.braille.dictionnaire
 {
-
-    public class MotAfficher
-    {
-        private static Dictionary<string, Statut> _statuts = new Dictionary<string, Statut> {
-            { "Inconnu", Statut.INCONNU },
-            { "Abréger", Statut.ABREGE },
-            { "Protéger", Statut.PROTEGE },
-            { "Ignorer", Statut.IGNORE }
-        };
-        private static Dictionary<Statut, string> _revert = _statuts.ToDictionary((kvp) => kvp.Value, (kvp) => kvp.Key);
-        public string[] StatutsPossible { get => _statuts.Keys.ToArray(); }
-
-        public string StatutChoisi { 
-            get => _revert[Statut]; 
-            set { 
-                Statut = _statuts[value];
-            }
-        }
-
-        public string Texte { get; set; }
-
-        public string ContexteAvant { get; set; } = null;
-        public string ContexteApres { get; set; } = null;
-        public string Contexte { get; set; } = null;
-        public Statut Statut { get; set; }
-
-        public int Index { get; set; }
-    }
     /// <summary>
     /// Logique d'interaction pour EditeurDictionnaireDeTravail.xaml
     /// </summary>
